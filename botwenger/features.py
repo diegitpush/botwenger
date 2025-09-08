784fe75eaa52a767224d5ceb4933dffbedac8303
diegitpush <97diegolopez@gmail.com>
2025-09-08 23:52:03 +0200

    features progress

--- botwenger/features.py
+++ botwenger/features.py
@@ -11,6 +11,23 @@ app = typer.Typer()
 
 class Features: 
 
+    preselected_features = ["player","season","player_red_card","player_non_penalti_goals","player_penalti_goals",
+                     "puntuacion_media_sofascore_as","player_price","minutes_played",
+                     "player_position","status","player_assists","player_second_yellow",
+                     "fixed_round"] #player/season to be removed later, used to calculate rolling features
+    
+    dummy_features = ["player_position","status_mapped"]
+
+    status_map = {
+    'discarded': 'ok',
+    'doubt': 'doubt',
+    'injured': 'injured',
+    'ok': 'ok',
+    'sanctioned': 'sanctioned',
+    'unknown': 'ok',
+    'warned': 'ok'
+    }
+
     @app.command()
     @staticmethod    
     def main():
@@ -61,7 +78,33 @@ class Features:
         group['player_price'] = group['player_price'].round().astype(int)
 
         return group
+    
+    @staticmethod
+    def curate_and_simplify_features(data: pd.DataFrame) -> pd.DataFrame:
+        logger.info(f"Removing position = 5, as they are coaches...")
+        data_filtered = data[data["player_position"].isin([1,2,3,4])]
 
+        logger.info(f"Simplifying status...")
+        data_filtered["status_mapped"] = data_filtered["status"].map(Features.status_map)
+
+        data_filtered = data_filtered.drop(columns=['status'])
+
+        return data_filtered
+
+    
+    @staticmethod    
+    def prefilter_features_to_use(data: pd.DataFrame) -> pd.DataFrame:
+        logger.info(f"Preselecting features in {Features.preselected_features}...")
+        data = data[Features.preselected_features] 
+        return data
+    
+
+    @staticmethod    
+    def create_dummies(data: pd.DataFrame) -> pd.DataFrame:
+        logger.info(f"Creating dummies for status field...")
+        data = pd.get_dummies(data, columns=Features.dummy_features)
+        return data
+    
 
 if __name__ == "__main__":
     app()
--- tests/test_features.py
+++ tests/test_features.py
@@ -5,13 +5,14 @@ from loguru import logger
 
 data = Features.loading_preprocessed_data(f"{INTERIM_DATA_DIR}/{INTERIM_DATA_FILENAME}")
 data_filled = Features.fill_fields_with_nas_for_basic_values(data)
-
+data_filled_market = data_filled.groupby(['player', 'season'], group_keys=False).apply(Features.fill_market_price)
+data_preselected_features = Features.prefilter_features_to_use(data_filled_market)
+data_curated = Features.curate_and_simplify_features(data_preselected_features)
+data_dummies = Features.create_dummies(data_curated)
 
 def test_basic_features_filling():
 
     logger.info("Basic checks for filled values")
-
-    data_filled = Features.fill_fields_with_nas_for_basic_values(data)
 
     assert data_filled["away_team_goals"].notna().all()
     assert data_filled["home_team_goals"].notna().all()  
@@ -22,7 +23,7 @@ def test_basic_features_filling():
 def test_fill_market_price():
 
     logger.info("Testing filled market prices")
-    data_filled_market = data_filled.groupby(['player', 'season'], group_keys=False).apply(Features.fill_market_price)
+
     assert data_filled_market["player_price"].notna().all()
 
     assert data_filled_market[(data_filled_market["player"]=="a-fernandez") & 
@@ -48,13 +49,29 @@ def test_fill_market_price():
     assert data_filled_market[(data_filled_market["player"]=="a-gorosabel") & 
                        (data_filled_market["season"]==2025) & 
                        (data_filled_market["fixed_round"]==38)]["player_price"].iloc[0] == 670000
+
+def test_curate_and_simplify_features():
+
+    logger.info("Testing cutating and simplifying features...")
+
+    assert len(data_curated[data_curated["player_position"]==5]) == 0
+    assert data_curated["status_mapped"].unique().size == 4
+    assert "status" not in data_curated.columns
     
 
-    
-    
-    
+def test_create_dummies_for_status():
 
+    logger.info("Testing the creation of dummies...")
 
+    assert "status_mapped" not in data_dummies.columns
+    assert "player_position" not in data_dummies.columns
 
-    
+    assert "status_mapped_ok" in data_dummies.columns
+    assert "status_mapped_injured" in data_dummies.columns
+    assert "status_mapped_sanctioned" in data_dummies.columns
+    assert "status_mapped_doubt" in data_dummies.columns
 
+    assert "player_position_1" in data_dummies.columns
+    assert "player_position_2" in data_dummies.columns
+    assert "player_position_3" in data_dummies.columns
+    assert "player_position_4" in data_dummies.columns
